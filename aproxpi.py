e051657f332d793f42cb068b62595668c352f4c5
brunoramosmartins <brunoramosmartins1989@gmail.com>
2023-02-16 17:28:51 -0300

    Solução interessante para encontrar aproximação do pi.

---
+++ aproxpi.py
@@ -0,0 +1,27 @@
+import turtle
+import math
+import random
+
+"""
+   Neste laboratório, vamos aproximar o valor de pi usando uma técnica conhecida
+   como Simulação de Monte Carlo. Isso significa que usaremos números aleatórios
+   para simular um jogo de azar. O resultado desse jogo será uma aproximação para pi.
+
+"""
+
+fred = turtle.Turtle()
+fred.up()
+
+wn = turtle.Screen()
+wn.setworldcoordinates(-1, -1, 1, 1)
+
+numdarts = 10
+
+for i in range(numdarts):
+    randx = random.random()
+    randy = random.random()
+
+    x = 
+    y =
+
+wn.exitonclick()
